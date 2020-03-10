3ef54f8f1c1f75747331c9861cb27c57bb885fe9
cassie <cassie866@163.com>
2020-03-10 11:56:10 +0800

    ShellSort.cpp

--- sf/ShellSort.cpp
+++ sf/ShellSort.cpp
@@ -30,17 +30,7 @@ void ShellSort(vector<int>&v)
 
 int main()
 {
-	vector<int> v;
-	v.push_back(3);
-	v.push_back(5);
-	v.push_back(7);
-	v.push_back(2);
-	v.push_back(4);
-	v.push_back(8);
-	v.push_back(1);
-	v.push_back(0);
-	v.push_back(9);
-	v.push_back(6);
+	vector<int> v={2,5,7,1,3,0,9,8,4,6};
 	ShellSort(v);
 	for(auto &e:v)
 	{
